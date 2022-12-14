6218aa6279c0bcbb9d3313bc48d68acdc421c334
Preston Moore <moorepjay@gmail.com>
2022-12-14 18:17:47 -0500

    MVP

--- app.py
+++ app.py
@@ -1,23 +1,33 @@
 import datetime
 from flask import Flask, render_template, request
-from PyMongo import MongoClient
-
-app = Flask(__name__)
-
-entries = []
+from pymongo import MongoClient
+import certifi
 
 
-@app.route('/', methods=["GET", "POST"])
-def home():
-    if request.method == "POST":
+def create_app():
+    app = Flask(__name__)
+    # Set up mongodb client
+    # Added certifi snippet
+    client = MongoClient("mongodb+srv://superUser:HurleyShark1441|@initflask.yjeyivn.mongodb.net/test",
+                         tlsCAFile=certifi.where())
+    app.db = client.Microblog
 
-        entry_content = request.form.get("content")
-        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+    @app.route('/', methods=["GET", "POST"])
+    def home():
+        if request.method == "POST":
+            entry_content = request.form.get("content")
+            formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+            app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
 
-        entries.append((entry_content, formatted_date))
+        entries_with_date = [
+            (
+                entry["content"],
+                entry["date"],
+                datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
+            )
+            for entry in app.db.entries.find({})
+        ]
 
-    return render_template("home.html", entries=entries)
+        return render_template("home.html", entries=entries_with_date)
 
-
-if __name__ == '__main__':
-    app.run()
+    return app
